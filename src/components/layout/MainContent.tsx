a8379c0f3f0bdf4051448f626276b6e4e18cf3ce
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-07-15 00:42:15 +0800

    Update MainContent.tsx

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import { useLinksContext } from '../../contexts/LinksContext';
 import { useCategoriesContext } from '../../contexts/CategoriesContext';
 import { useConfigContext } from '../../contexts/ConfigContext';
